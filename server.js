58cf7ccf3d83a739d6d76ff2297c4b5b0e1bf91e
jesscru <jessicacrump7@gmail.com>
2021-01-15 23:37:41 -0700

    fixed file structure and created empty requests in server.js file

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
+// Dependencies
+// =============================================================
 var express = require("express");
-const apiRoutes = require("./routes/apiroutes");
-const apiRoutes2 = require("./routes/htmlroutes");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = 3000;
 
-
-app.use(express.json());
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
 
-app.use("/api", apiRoutes);
-app.use("/", apiRoutes2);
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
 
 
+// Displays all characters
+app.get("/api/characters", function(req, res) {
+  
+});
+
+// Displays all saved notes
+app.get("/api/notes", function(req, res) {
+  
+});
+
+// Create new notes - takes in JSON input
+app.post("/api/notes", function(req, res) {
+
+});
+
+// Deletes requested note via an id given to the note 
+app.delete("/api/notes/:id", function(req, res) {
+
+});
 
 
-
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
