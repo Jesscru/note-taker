f511ab53931f85d4dce18168b0aac5c7863af239
jesscru <jessicacrump7@gmail.com>
2021-01-15 23:52:54 -0700

    completed route to api/notes to send back all saved notes

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const fs = require('fs');
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,25 +17,19 @@ app.use(express.json());
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
-
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  
-});
 
 // Displays all saved notes
 app.get("/api/notes", function(req, res) {
-  
+  res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 // Create new notes - takes in JSON input
