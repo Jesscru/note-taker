fbd1b2133b786578155e9afd6fed355adda96bdb
jesscru <jessicacrump7@gmail.com>
2021-01-16 01:28:08 -0700

    updated port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var db = require("./db/db.json");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
